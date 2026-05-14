2d5e10381f2c7fb1f37fd24405a6f51ee16e55ae
Tonsagoon Jomdoung <tonsagoonjomdoung@192.168.1.5>
2026-05-14 11:52:33 +0700

    feat: restore Facebook page iframe in footer

--- src/components/home/home-footer.tsx
+++ src/components/home/home-footer.tsx
@@ -29,6 +29,19 @@ export function HomeFooter() {
               <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>
               Facebook
             </a>
+            <div className="mt-2">
+              <iframe
+                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fselloutthailand&tabs=timeline&width=300&height=150&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true"
+                width="300"
+                height="150"
+                style={{ border: "none", overflow: "hidden" }}
+                scrolling="no"
+                frameBorder={0}
+                allowFullScreen
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                title="เซ้งร้าน.com Facebook"
+              />
+            </div>
           </div>
 
           {/* Contact */}
